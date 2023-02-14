df1e1cf064c2cfc9b3d8281d438caa8222daa667
Austin Jones <amjones1@pointpark.edu>
2023-02-13 19:28:29 -0500

    Completed Programming Assignment 6

---
+++ Programming_Assignment_6/js/hideimage.js
@@ -0,0 +1,15 @@
+function hideS(){
+    document.getElementById('modelS').className = "hidden";
+}
+function hide3(){
+    document.getElementById('model3').className = "hidden";
+}
+function hideY(){
+    document.getElementById('modelY').className = "hidden";
+}
+function hideX(){
+    document.getElementById('modelX').className = "hidden";
+}
+function hideR(){
+    document.getElementById('roadster').className = "hidden";
+}
---
+++ Programming_Assignment_6/js/modifiying_text.js
@@ -0,0 +1,19 @@
+function enlargeTextT(){
+    document.getElementById('h1').style.fontSize = "5em";
+
+}
+
+function decreaseTextT(){
+    document.getElementById('h1').style.fontSize = "1em";
+
+}
+
+function enlargeTextH(){
+    document.getElementById('h2').style.fontSize = "5em";
+
+}
+
+function decreaseTextH(){
+    document.getElementById('h2').style.fontSize = "1em";
+
+}
